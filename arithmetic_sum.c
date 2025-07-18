5d1927380882acd63fb1757f629ad0ffd1a2df10
ANKITA GUPTA <141404641+ankita430@users.noreply.github.com>
2025-07-18 08:40:52 +0000

    added arithmetic sum

---
+++ arithmetic_sum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;  
+     int sum;
+
+    printf("Enter any two numbers: ");
+    scanf("%d%d", &num1, &num2);
+
+    sum  = num1 + num2;
+
+    printf("SUM = %d\n", sum);
+
+     return 0;
+}
