f719b99b60a18a2c30ab8dbb5096d096d15bc9ab
ANKITA GUPTA <141404641+ankita430@users.noreply.github.com>
2025-07-18 08:56:31 +0000

    added arithmetic mult

---
+++ arithmetic_mult.c
@@ -0,0 +1,14 @@
+int main()
+{
+    int num1, num2;            
+    int mult;   
+
+    printf("Enter any two numbers: ");
+    scanf("%d%d", &num1, &num2);
+
+    mult = num1 * num2;
+   
+    printf("PRODUCT = %d\n", mult);
+
+    return 0;
+}
