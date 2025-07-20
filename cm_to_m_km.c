01ef088ff8eb5c1ef51311209851c37b5dee04fc
ANKITA GUPTA <141404641+ankita430@users.noreply.github.com>
2025-07-20 23:46:42 +0530

    Update cm_to_m_km.c

--- cm_to_m_km.c
+++ cm_to_m_km.c
@@ -4,7 +4,7 @@ int main()
     float cm, m, km;
     printf("cm: ");
     scanf("%f", &cm);
-    printf("m = %.2f m, km = %.2f km\n", cm/100, cm/100000);
+    printf("m = %.2f m\n km = %.2f km\n", cm/100, cm/100000);
     
     return 0;
 }
