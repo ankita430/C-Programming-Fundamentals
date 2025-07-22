9557a430346bcc667abea961ddb54771077ef5c3
ANKITA GUPTA <141404641+ankita430@users.noreply.github.com>
2025-07-22 23:26:13 +0530

    Update char_io.c

--- char_io.c
+++ char_io.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
-int main(void)
+
+int main()
 {
     char grade;
-    printf("Enter student grade: ");
-    grade = getchar();
-    printf("You entered: ");
-    putchar(grade);1
-    printf("\n");
+
+    printf("grade: ");
+    scanf("%c", &grade); 
+
+    printf("grade: %c\n", grade);
 
     return 0;
 }
+
+
+
+
+
