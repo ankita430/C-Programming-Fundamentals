4f457296c42fc3425511a658df2b5d641a7f0876
ANKITA GUPTA <141404641+ankita430@users.noreply.github.com>
2025-07-23 10:26:20 +0530

    Update integers_types_io.c

--- integers_types_io.c
+++ integers_types_io.c
@@ -2,50 +2,40 @@
 
 int main()
 {
-    short shortVal;
-    unsigned short uShortVal;
-    int intVal;
-    unsigned int uIntVal;
-    long longVal;
-    unsigned long uLongVal;
-    long long longLongVal;
-    unsigned long long uLongLongVal;
+    short a;                    
+    unsigned short b;           
 
-     printf("Enter a signed short: ");
-    scanf("%hi", &shortVal);
+    int c;                      
+    unsigned int d;             
 
-    printf("Enter an unsigned short: ");
-    scanf("%hu", &uShortVal);
+    long e;                     
+    unsigned long f;            
+    long long g;                
+    unsigned long long h;       
 
-    printf("Enter a signed integer: ");
-    scanf("%d", &intVal);
+    
+    printf("a b c d e f g h: ");
+    scanf("%hi %hu %d %u %ld %lu %lld %llu", &a, &b, &c, &d, &e, &f, &g, &h);
 
-    printf("Enter an unsigned integer: ");
-    scanf("%u", &uIntVal);
+    
+    printf("short = %hi\n", a);
+    printf("unsigned short = %hu\n\n", b);
 
-    printf("Enter a signed long: ");
-    scanf("%ld", &longVal);
+    printf("int = %d\n", c);
+    printf("unsigned int = %u\n\n", d);
 
-    printf("Enter an unsigned long: ");
-    scanf("%lu", &uLongVal);
+    printf("long = %ld\n", e);
+    printf("unsigned long = %lu\n\n", f);
 
-    printf("Enter a signed long long: ");
-    scanf("%lld", &longLongVal);
-
-    printf("Enter an unsigned long long: ");
-    scanf("%llu", &uLongLongVal);
-
-    printf("You entered signed short: %hi\n", shortVal);
-    printf("You entered unsigned short: %hu\n\n", uShortVal);
-
-    printf("You entered signed int: %d\n", intVal);
-    printf("You entered unsigned int: %u\n\n", uIntVal);
-
-    printf("You entered signed long: %ld\n", longVal);
-    printf("You entered unsigned long: %lu\n\n", uLongVal);
-
-    printf("You entered signed long long: %lld\n", longLongVal);
-    printf("You entered unsigned long long: %llu\n\n", uLongLongVal);
+    printf("long long = %lld\n", g);
+    printf("unsigned long long = %llu\n", h);
 
     return 0;
 }
+
+
+
+   
+
+    
+   
