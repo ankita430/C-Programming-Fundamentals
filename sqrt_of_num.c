4de6d56af4fa7cfc168f453b09153f29cff42ca8
ANKITA GUPTA <141404641+ankita430@users.noreply.github.com>
2025-07-24 21:16:08 +0530

    Update sqrt_of_num.c

--- sqrt_of_num.c
+++ sqrt_of_num.c
@@ -14,4 +14,22 @@ int main()
 
     return 0;
 }
+----*----
+
+#include <stdio.h>
+#include <math.h>
+int main() {
+    // Write C code here
+    int i;
+    printf("enter value of i: ");
+    scanf("%d", &i);
+    int res;
+    
+    res = sqrt(i);
+    
+    printf("%d", res);
+    
+    
+    return 0;
+}
 
