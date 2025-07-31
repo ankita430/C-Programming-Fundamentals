59ed80ce9c111d0d0f2652a5789049931d9e09a0
ANKITA GUPTA <141404641+ankita430@users.noreply.github.com>
2025-07-31 19:44:11 +0530

    Create check_prime_num.c

---
+++ check_prime_num.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int num = 11;
+    int fact = 0;
+    for(int i = 1; i <= num; i++)
+    {
+        if(num%i == 0)
+        fact++;
+    }
+    if (fact == 2)
+    printf("Given %d number is a prime number", num);
+    else
+    printf("Not a prime number");
+return 0;
+}
