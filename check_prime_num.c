35615d0c00080750374da05b44b4111fe2b0eaca
ANKITA GUPTA <141404641+ankita430@users.noreply.github.com>
2025-08-05 10:47:05 +0530

    Update check_prime_num.c

--- check_prime_num.c
+++ check_prime_num.c
@@ -15,31 +15,3 @@ int main()
 return 0;
 }
 
-------*------
-#include <stdio.h>
-int main()
-{
-    int num, isPrime, i;
-    printf("Enter any number: ");
-    scanf("%d", &num);
-    isPrime = 1;
-    for(i = 2; i<num; i++)
-    {
-        if(num%i == 0)
-        {
-            isPrime = 0;
-            break;
-        }
-    }
-        if(isPrime == 1)
-        {
-            printf("Number is prime number.");
-        }
-        else
-        {
-            printf("Number is composite number.");
-        }
-        return 0;
-}
-
-
